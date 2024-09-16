c4764b93a90c1184205dba655aeed7e4c81ef7c6
roum02 <roum02@ewhain.net>
2024-09-16 23:59:11 +0900

    Add type [main]

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-const app = express();
+import express, {Application} from 'express';
+
+const app: Application = express();
 const port = 3000;
-app.use(express.json());
-app.use(express.static("public"));
-app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    return console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 });
+
+export default app;
