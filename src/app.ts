0af1d489afccd4a3502e589f795e1f4fb4fb8829
roum02 <roum02@ewhain.net>
2024-09-17 23:44:42 +0900

    Add image test url [main]

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,13 @@ import express, {Application} from 'express';
 const app: Application = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/ping', (req, res) => {
+    res.send('pong');
 });
+
+app.get('/image', (req, res) => {
+    res.send(`this is an image test`);
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
