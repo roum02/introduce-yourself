3f033c87969d0370e0956c0af877008acd3ab7d6
roum02 <roum02@ewhain.net>
2024-09-29 23:52:23 +0900

    Modify pnpm setting [main]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import babelParser from '@babel/eslint-parser'; // Babel 파서 추가
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['node_modules/**'], // ESLint에서 무시할 파일들 설정
+    languageOptions: {
+      globals: globals.browser,
+      parser: babelParser, // Babel 파서를 ESLint에서 사용
+      parserOptions: {
+        requireConfigFile: false, // Babel 설정 파일이 필요하지 않도록 설정
+        babelOptions: {
+          presets: ['@babel/preset-env', '@babel/preset-typescript'],
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+    },
+  },
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
-import express, {Application} from 'express';
+import express, { Request, Response, Application } from 'express';
+
 
 const app: Application = express();
 const port = 3000;
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (req: Request, res: Response) => {
     res.send('pong');
 });
 
-app.get('/name-tag', (req, res) => {
+app.get('/name-tag', (req: Request, res: Response) => {
     res.send(`
     <svg xmlns="http://www.w3.org/2000/svg">
-        <rect width="500" height="300"></rect>
+        <rect width="500" height="300" fill="#3d87fb"></rect>
+        <text 
+        x="30" y="90" 
+        fill="#ED6E46" font-size="100" font-family="'Leckerli One', cursive">
+        hello
+        </text>
     </svg>`);
-})
+});
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
