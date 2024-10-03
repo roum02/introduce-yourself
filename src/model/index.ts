da3ed9fb9b2cf1b276eb5caa5dcba4d1b642a470
roum02 <roum02@ewhain.net>
2024-10-03 23:59:56 +0900

    Add textRender [main]

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,9 +1,7 @@
+import { handleTextRender } from './textModel';
+
 export const renderModel = {
-  text: {
-    render: () => `<text
-                x="30" y="90"
-                fill="#ED6E46" font-size="100" font-family="'Leckerli One', cursive">
-                text
-                </text>`,
-  },
+	text: {
+		render: () => handleTextRender('hello'),
+	},
 };
---
+++ src/model/textModel.ts
@@ -0,0 +1,7 @@
+export const handleTextRender = (text: string) => {
+	return `<text
+                x="30" y="90"
+                fill="#ED6E46" font-size="100" font-family="'Leckerli One', cursive">
+                text
+                </text>`;
+};
